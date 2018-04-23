a33764bc832fef3f36dc6bca6aca928183223014
paul <7328194@163.com>
2018-04-23 17:30:28 +0800

    update demo

--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import App from '@containers/app/index';
 
 export default () => (
   <Router>
-    <App />
+    <Route path='/' component={App} />
   </Router>
 )
