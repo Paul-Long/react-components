388938532da835271c95053a561a95f68d1497f2
paul <7328194@163.com>
2018-06-06 13:28:15 +0800

    update

--- src/client/containers/calendar/basic/index.js
+++ src/client/containers/calendar/basic/index.js
@@ -4,9 +4,17 @@ import Calendar, { CalendarType } from 'rcc-calendar';
 class Basic extends React.PureComponent {
   render() {
     return (
-      <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <h2>基本</h2>
         <Calendar />
-        <Calendar calenderType={CalendarType.ICON} />
+        <h2>Icon Calendar</h2>
+        <Calendar calendarType={CalendarType.ICON} />
       </div>
     );
   }
--- src/client/containers/calendar/range/index.js
+++ src/client/containers/calendar/range/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Calendar, {PickerType} from 'rcc-calendar';
+import Calendar, { PickerType } from 'rcc-calendar';
 
 class RangeDate extends React.PureComponent {
   render() {
     return (
-      <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div style={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <h2>基本</h2>
         <Calendar pickerType={PickerType.RANGE} />
       </div>
     );
   }
 }
+
 export default RangeDate;
